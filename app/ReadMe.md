8b187726565b90841841d25ff0e5a063a63b49f3
Geraldine <grcentinaje@gmail.com>
2019-05-16 00:06:06 -0700

    friend finder

---
+++ app/ReadMe.md
@@ -0,0 +1,13 @@
+# Friend Finder - Node and Express Servers
+
+### Overall View
+
+You will be presented with 10 questions that you must rate from 1 to 5, where 1 signifies that you agree the least, and 5 that signifies you agree the most. At the end of the survey, based on your responses and other people's responses that have taken this survey, is calculated to find the person that is most compatible with you.
+
+1. Access the survey by starting the app using the node server in your terminal, then clicking the button "Start Survey" in your browser.
+
+2. Answer the 10 questions given to you, with 1 signifying that you strongly disagree, and 5 signifying that you strongly agree.
+
+3. At the end of the survey, a modal will appear to show you your results.
+
+Have fun and good luck!
---
+++ app/data/friends.js
@@ -0,0 +1,89 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends.
+// ===============================================================================
+
+var friendsArray = [
+    {
+        "name":"Theresa",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+
+      {
+        "name":"Cera",
+        "scores":[
+            3,
+            2,
+            1,
+            4,
+            3,
+            3,
+            3,
+            3,
+            2,
+            1
+          ]
+      },
+
+      {
+        "name":"Danika",
+        "scores":[
+            3,
+            1,
+            5,
+            5,
+            4,
+            2,
+            3,
+            4,
+            2,
+            2
+          ]
+      },
+
+      {
+        "name":"Jordan",
+        "scores":[
+            2,
+            2,
+            2,
+            4,
+            3,
+            4,
+            1,
+            4,
+            5,
+            5
+          ]
+      },
+
+      {
+        "name":"Laura",
+        "scores":[
+            5,
+            5,
+            5,
+            4,
+            4,
+            4,
+            3,
+            3,
+            2,
+            1
+          ]
+      },
+    ]
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
